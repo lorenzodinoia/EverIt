f501cfa8d9e0b31f55a0f41a8e7bd7ed640dafeb
Nicola Cassetta <commento_9@libero.it>
2020-01-23 19:57:24 +0100

    Register restaurateur rider

--- app/src/main/java/it/uniba/di/sms1920/everit/RegisterActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/everit/RegisterActivity.java
@@ -5,40 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText editTextName;
     private EditText editTextSurname;
-    private EditText editTextPhone;
-    private EditText editTextEmail;
-    private EditText editTextPassword;
     private Button buttonRegister;
+    private EditText editTextPhoneNumber;
+    private EditText editTextMail;
+    private EditText editTextPassword;
     private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
         this.initComponents();
     }
 
     private void initComponents() {
-        this.editTextEmail =  findViewById(R.id.editTextEmail);
+        this.editTextMail =  findViewById(R.id.editTextMail);
         this.editTextPassword = findViewById(R.id.editTextPassword);
         this.buttonLogin = findViewById(R.id.buttonLogin);
-        this.editTextName = findViewById(R.id.editTextName);
-        this.editTextSurname = findViewById(R.id.editTextSurname);
-        this.editTextPhone = findViewById(R.id.editTextPhone);
-        this.buttonRegister = findViewById(R.id.buttonRegister);
+
+//        this.editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+//        this.editTextName = findViewById(R.id.ediTextName);
+//        this.editTextSurname = findViewById(R.id.ediTextSurname);
+//        this.buttonRegister = findViewById(R.id.buttonRegister);
+
 
         this.buttonRegister.setOnClickListener(view -> {
-            String email = this.editTextEmail.getText().toString();
+            String email = this.editTextMail.getText().toString();
             String password = this.editTextPassword.getText().toString();
             String name = this.editTextName.getText().toString();
             String surname = this.editTextSurname.getText().toString();
-            String phone = this.editTextPhone.getText().toString();
+            String phone = this.editTextPhoneNumber.getText().toString();
         });
     }
 
---
+++ app/src/restaurateur/java/it/uniba/di/sms1920/everit/restaurateur/RegisterActivity.java
@@ -0,0 +1,77 @@
+package it.uniba.di.sms1920.everit.restaurateur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import it.uniba.di.sms1920.everit.R;
+
+public class RegisterActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
+
+    private EditText editTextNameAct;
+    private EditText editTextAddress;
+    private EditText editTextPhone;
+    private EditText editTextPIVA;
+    private EditText editTextActivityDescription;
+    private EditText editTextMail;
+    private EditText editTextPassword;
+    private Button buttonContinue;
+    private Button buttonFinish;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        initComponents();
+
+        Spinner spinner = findViewById(R.id.spinnerActivityType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+
+    private void initComponents() {
+        this.editTextMail =  findViewById(R.id.editTextMail);
+        this.editTextNameAct = findViewById(R.id.editTextNameAct);
+        this.editTextAddress = findViewById(R.id.editTextAddress);
+        this.buttonContinue = findViewById(R.id.buttonContinue);
+
+        this.buttonContinue.setOnClickListener( view -> {
+            setContentView(R.layout.activity_register_p2);
+            initComponentsP2();
+        });
+
+    }
+
+    private void initComponentsP2(){
+        this.editTextPassword = findViewById(R.id.editTextPhone);
+        this.editTextPhone = findViewById(R.id.editTextPhone);
+        this.editTextPIVA = findViewById(R.id.editTextPIVA);
+        this.editTextActivityDescription = findViewById(R.id.editTextDescription);
+        this.buttonFinish = findViewById(R.id.buttonEnd);
+    }
+
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
+}
