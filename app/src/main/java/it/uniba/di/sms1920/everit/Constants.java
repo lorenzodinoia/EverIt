c4f193267c707e3a229aa7ba84ecd020a4a37bfa
Lorenzo Di Noia <lorenzodn.98@gmail.com>
2020-01-30 20:41:21 +0100

    Register device id for notifications. Encrypted preferences file implemented for user credentials

--- app/src/main/java/it/uniba/di/sms1920/everit/Constants.java
+++ app/src/main/java/it/uniba/di/sms1920/everit/Constants.java
@@ -3,9 +3,22 @@ package it.uniba.di.sms1920.everit;
 public final class Constants {
     public static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
     public static final String SERVER_HOST = "https://everitsms.000webhostapp.com";
-    public static final String FLAVOR_CUSTOMER = "customer";
-    public static final String FLAVOR_RESTAURATEUR = "restaurateur";
-    public static final String FLAVOR_RIDER = "rider";
+    public static final String PREFS_ENC_FILE_NAME = "EverIt_Credentials";
     public static final String PREFS_FILE_NAME = "EverIt_Preferences";
-    public static final String PREFS_APP_TOKEN_KEY = "app_token";
+
+    public final static class Flavors {
+        public static final String CUSTOMER = "customer";
+        public static final String RESTAURATEUR = "restaurateur";
+        public static final String RIDER = "rider";
+    }
+
+    public final static class SharedPreferencesKeys {
+        public static final String APP_TOKEN = "app_token";
+        public static final String REMEMBER_ME = "remember_me";
+    }
+
+    public final static class EncSharedPreferencesKeys {
+        public static final String EMAIL = "email";
+        public static final String PASSWORD = "password";
+    }
 }
--- app/src/main/java/it/uniba/di/sms1920/everit/adapter/AdapterProvider.java
+++ app/src/main/java/it/uniba/di/sms1920/everit/adapter/AdapterProvider.java
@@ -7,6 +7,8 @@ import it.uniba.di.sms1920.everit.models.City;
 import it.uniba.di.sms1920.everit.models.Customer;
 import it.uniba.di.sms1920.everit.models.Model;
 import it.uniba.di.sms1920.everit.models.Order;
+import it.uniba.di.sms1920.everit.models.Restaurateur;
+import it.uniba.di.sms1920.everit.models.Rider;
 
 public final class AdapterProvider {
     private static final Map<Class<? extends Model>, Adapter<? extends Model>> adapters = new HashMap<>();
@@ -15,6 +17,8 @@ public final class AdapterProvider {
         adapters.put(Customer.class, new Adapter<Customer>());
         adapters.put(City.class, new Adapter<City>());
         adapters.put(Order.class, new Adapter<Order>());
+        adapters.put(Restaurateur.class, new Adapter<Restaurateur>());
+        adapters.put(Rider.class, new Adapter<Rider>());
     }
 
     public static <T extends Model> Adapter<T> getAdapterFor(Class<T> modelClass) {
