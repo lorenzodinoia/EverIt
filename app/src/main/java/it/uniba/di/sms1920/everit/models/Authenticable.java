4419ba91513203b7ab293e65a01a301569315abc
Lorenzo Di Noia <lorenzodn.98@gmail.com>
2020-04-10 12:20:00 +0200

    Deleted all files

--- app/src/main/java/it/uniba/di/sms1920/everit/models/Authenticable.java
+++
@@ -1,3 +0,0 @@
-package it.uniba.di.sms1920.everit.models;
-
-public interface Authenticable {}
--- app/src/main/java/it/uniba/di/sms1920/everit/request/RequestListener.java
+++
@@ -1,6 +0,0 @@
-package it.uniba.di.sms1920.everit.request;
-
-public interface RequestListener<T> {
-    void successResponse(T response);
-    void errorResponse(String error);
-}
