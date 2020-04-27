fdc4878abb882e8d16fc62ff5e001b521a89b31a
Lorenzo Di Noia <lorenzodn.98@gmail.com>
2020-04-27 21:06:26 +0200

    Restaurant search activity: list and list item layout

---
+++ customer/src/main/java/it/uniba/di/sms1920/everit/customer/activities/results/ResultListActivity.java
@@ -0,0 +1,207 @@
+package it.uniba.di.sms1920.everit.customer.activities.results;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import it.uniba.di.sms1920.everit.customer.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * An activity representing a list of Results. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link ResultDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class ResultListActivity extends AppCompatActivity {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if (findViewById(R.id.result_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        View recyclerView = findViewById(R.id.result_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
+    }
+
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final ResultListActivity mParentActivity;
+        private final List<DummyContent.DummyItem> mValues;
+        private final boolean mTwoPane;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DummyContent.DummyItem item = (DummyContent.DummyItem) view.getTag();
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(ResultDetailFragment.ARG_ITEM_ID, item.id);
+                    ResultDetailFragment fragment = new ResultDetailFragment();
+                    fragment.setArguments(arguments);
+                    mParentActivity.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.result_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, ResultDetailActivity.class);
+                    intent.putExtra(ResultDetailFragment.ARG_ITEM_ID, item.id);
+
+                    context.startActivity(intent);
+                }
+            }
+        };
+
+        SimpleItemRecyclerViewAdapter(ResultListActivity parent,
+                                      List<DummyContent.DummyItem> items,
+                                      boolean twoPane) {
+            mValues = items;
+            mParentActivity = parent;
+            mTwoPane = twoPane;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.result_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            //holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        //TODO Sistemare View
+        class ViewHolder extends RecyclerView.ViewHolder {
+            //final TextView mIdView;
+            final TextView mContentView;
+
+            ViewHolder(View view) {
+                super(view);
+                //mIdView = (TextView) view.findViewById(R.id.id_text);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+        }
+    }
+
+    public static class DummyContent {
+
+        /**
+         * An array of sample (dummy) items.
+         */
+        public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+        /**
+         * A map of sample (dummy) items, by ID.
+         */
+        public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+        private static final int COUNT = 25;
+
+        static {
+            // Add some sample items.
+            for (int i = 1; i <= COUNT; i++) {
+                addItem(createDummyItem(i));
+            }
+        }
+
+        private static void addItem(DummyItem item) {
+            ITEMS.add(item);
+            ITEM_MAP.put(item.id, item);
+        }
+
+        private static DummyItem createDummyItem(int position) {
+            return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
+        }
+
+        private static String makeDetails(int position) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("Details about Item: ").append(position);
+            for (int i = 0; i < position; i++) {
+                builder.append("\nMore details information here.");
+            }
+            return builder.toString();
+        }
+
+        /**
+         * A dummy item representing a piece of content.
+         */
+        public static class DummyItem {
+            public final String id;
+            public final String content;
+            public final String details;
+
+            public DummyItem(String id, String content, String details) {
+                this.id = id;
+                this.content = content;
+                this.details = details;
+            }
+
+            @Override
+            public String toString() {
+                return content;
+            }
+        }
+    }
+}
