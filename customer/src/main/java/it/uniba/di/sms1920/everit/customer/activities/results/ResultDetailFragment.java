66a7ebd09d3c2f03658561a52f2a9b5ae4fc32a5
Lorenzo Di Noia <lorenzodn.98@gmail.com>
2020-05-09 20:32:02 +0200

    RecyclerView Adapter for search result list

--- customer/src/main/java/it/uniba/di/sms1920/everit/customer/activities/results/ResultDetailFragment.java
+++ customer/src/main/java/it/uniba/di/sms1920/everit/customer/activities/results/ResultDetailFragment.java
@@ -1,72 +1,38 @@
 package it.uniba.di.sms1920.everit.customer.activities.results;
 
-import android.app.Activity;
 import android.os.Bundle;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import it.uniba.di.sms1920.everit.customer.R;
-import it.uniba.di.sms1920.everit.customer.activities.dummy.DummyContent;
+import it.uniba.di.sms1920.everit.utils.models.Restaurateur;
 
-/**
- * A fragment representing a single Result detail screen.
- * This fragment is either contained in a {@link ResultListActivity}
- * in two-pane mode (on tablets) or a {@link ResultDetailActivity}
- * on handsets.
- */
+
 public class ResultDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
     public static final String ARG_ITEM_ID = "item_id";
+    private Restaurateur restaurateur;
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
-    private DummyContent.DummyItem mItem;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public ResultDetailFragment() {
-    }
+    public ResultDetailFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
-
-            Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-            if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
-            }
+        if ((getArguments() != null) && (getArguments().containsKey(ARG_ITEM_ID))) {
+            long id = getArguments().getLong(ARG_ITEM_ID);
+            this.restaurateur = ResultListActivity.getResultById(id);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.result_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.result_detail)).setText(mItem.details);
-        }
+        //TODO Mapping dati ristorante
 
         return rootView;
     }
