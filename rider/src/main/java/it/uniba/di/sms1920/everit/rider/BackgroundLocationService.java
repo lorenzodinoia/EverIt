ffaa1c427b442b3a907d6a625725958564894aae
Lorenzo Di Noia <lorenzodn.98@gmail.com>
2020-07-15 10:09:21 +0200

    BackgroundLocationService AIDL and binding

--- rider/src/main/java/it/uniba/di/sms1920/everit/rider/BackgroundLocationService.java
+++ rider/src/main/java/it/uniba/di/sms1920/everit/rider/BackgroundLocationService.java
@@ -1,12 +1,19 @@
 package it.uniba.di.sms1920.everit.rider;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
 import android.os.IBinder;
 import android.os.Looper;
+import android.os.RemoteException;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -14,47 +21,120 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import it.uniba.di.sms1920.everit.rider.activities.BaseActivity;
+
 public class BackgroundLocationService extends Service {
-    private static final long LOCATION_UPDATES_INTERVAL = 2000;
-    private static final int LOCATION_UPDATES_PRIORITY = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY;
-    
+    public static final String ACTION_START_WORKING = "service.work";
+
+    private static final class LocationUpdatesSettings {
+        private static final long INTERVAL = 10000;
+        private static final int PRIORITY = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY;
+        private static final float MIN_DISPLACEMENT = 50f;
+    }
+
+    private static final class NotificationSettings {
+        private static final String CHANNEL_NAME = "Location updates service";
+        private static final String CHANNEL_ID = "Location-Service";
+        private static final int ID = 673;
+    }
+
     private static LocationRequest locationRequestSettings;
 
+    private boolean working;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private LocationCallback locationCallback;
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if (locationResult != null) {
+                onNewLocation(locationResult.getLastLocation());
+            }
+        }
+    };
 
     public BackgroundLocationService() {
         locationRequestSettings = LocationRequest.create();
-        locationRequestSettings.setInterval(LOCATION_UPDATES_INTERVAL);
-        locationRequestSettings.setPriority(LOCATION_UPDATES_PRIORITY);
+        locationRequestSettings.setInterval(LocationUpdatesSettings.INTERVAL);
+        locationRequestSettings.setFastestInterval(LocationUpdatesSettings.INTERVAL);
+        locationRequestSettings.setSmallestDisplacement(LocationUpdatesSettings.MIN_DISPLACEMENT);
+        locationRequestSettings.setPriority(LocationUpdatesSettings.PRIORITY);
     }
 
     @Override
     public void onCreate() {
         this.fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        this.setLocationCallback();
-        this.startLocationUpdates();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException();
+        return new IBackgroundLocationService.Stub() {
+            @Override
+            public boolean isWorking() throws RemoteException {
+                return BackgroundLocationService.this.working;
+            }
+
+            @Override
+            public void stop() throws RemoteException {
+                BackgroundLocationService.this.stop();
+            }
+        };
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        if ((intent.getAction() != null) && (intent.getAction().equals(ACTION_START_WORKING))) {
+            this.startWorking();
+            Log.d("SERVIZIO", "Avviato");
+        }
+
+        return START_STICKY;
     }
 
-    private void setLocationCallback() {
-        this.locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult != null) {
-                    onNewLocation(locationResult.getLastLocation());
-                }
+    @Override
+    public void onDestroy() {
+        Log.d("SERVIZIO", "Distrutto");
+        this.stopWorking();
+        super.onDestroy();
+    }
+
+    private void initNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            final String channelName = NotificationSettings.CHANNEL_NAME;
+            final int channelLevel = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(NotificationSettings.CHANNEL_ID, channelName, channelLevel);
+            NotificationManager notificationManager = getApplicationContext().getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
             }
-        };
+        }
+    }
+
+    private Notification createNotification() {
+        this.initNotificationChannel();
+
+        Intent notificationIntent = new Intent(getApplicationContext(), BaseActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, NotificationSettings.CHANNEL_ID)
+                .setContentTitle(getApplicationContext().getString(R.string.notification_location_title))
+                .setContentText(getApplicationContext().getString(R.string.notification_location_body))
+                .setSmallIcon(R.mipmap.icon_round)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.flags = notification.flags | Notification.FLAG_NO_CLEAR;
+
+        return notification;
+    }
+
+    private void startWorking() {
+        this.startLocationUpdates();
+        Notification notification = this.createNotification();
+        this.working = true;
+        super.startForeground(NotificationSettings.ID, notification);
+    }
+
+    private void stopWorking() {
+        this.stopLocationUpdates();
+        this.working = false;
     }
 
     @SuppressLint("MissingPermission")
@@ -62,7 +142,15 @@ public class BackgroundLocationService extends Service {
         this.fusedLocationProviderClient.requestLocationUpdates(locationRequestSettings, locationCallback, Looper.getMainLooper());
     }
 
+    private void stopLocationUpdates() {
+        this.fusedLocationProviderClient.removeLocationUpdates(this.locationCallback);
+    }
+
     private void onNewLocation(Location location) {
-        Log.d("POSIZIONE", location.toString());
+        Log.d("SERVIZIO", location.toString());
+    }
+
+    public void stop() {
+        super.stopSelf();
     }
 }
--- rider/src/main/java/it/uniba/di/sms1920/everit/rider/activities/HomeFragment.java
+++ rider/src/main/java/it/uniba/di/sms1920/everit/rider/activities/HomeFragment.java
@@ -1,22 +1,41 @@
 package it.uniba.di.sms1920.everit.rider.activities;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.IBinder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ToggleButton;
 
+import it.uniba.di.sms1920.everit.rider.BackgroundLocationService;
+import it.uniba.di.sms1920.everit.rider.IBackgroundLocationService;
 import it.uniba.di.sms1920.everit.rider.R;
 
 public class HomeFragment extends Fragment {
-
+    private Intent locationServiceIntent;
+    private ServiceConnection serviceConnection;
+    private IBackgroundLocationService backgroundLocationService;
 
     public HomeFragment() {
-        // Required empty public constructor
-    }
+        this.locationServiceIntent = new Intent(getContext(), BackgroundLocationService.class);
+        this.serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                backgroundLocationService = IBackgroundLocationService.Stub.asInterface(service);
+            }
 
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                backgroundLocationService = null;
+            }
+        };
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +44,14 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View viewRoot = inflater.inflate(R.layout.fragment_home, container, false);
+        this.initComponent(viewRoot);
+        return viewRoot;
+    }
+
+    private void initComponent(View viewRoot) {
+        getContext().bindService(this.locationServiceIntent, this.serviceConnection, 0);
+
+        ToggleButton buttonService = viewRoot.findViewById(R.id.toggleButtonLocationService);
     }
 }
